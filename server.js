3d40afa60e2bbd4aa393e65746c9632992f756bb
Kamel Laïdi <101251010+kamellaidi@users.noreply.github.com>
2022-08-24 15:30:53 +0200

    Update CORS

--- server.js
+++ server.js
@@ -57,9 +57,11 @@ const db = {
 // parse request body
 app.use(bodyParser.json());
 
+// app.use(cors(process.env.CORS_DOMAIN ?? '*'));
+
 // cors
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
 
