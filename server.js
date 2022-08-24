5a30bfec00715d6feaf0041b795204faf952c7c2
Kamel Laïdi <101251010+kamellaidi@users.noreply.github.com>
2022-08-24 15:35:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.use(bodyParser.json());
 
 // cors
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://reactrecipeskamel.herokuapp.com/');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
 
